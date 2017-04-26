a8d059bac2ee9e0b70050ad2dd889399f6d14e84
Oskar Pol <oskarbank@gmail.com>
2017-04-26 19:39:02 +0200

    mysql

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
-public class Controller {
+
+public class Controller implements Initializable {
 
 
     @FXML
@@ -40,7 +48,41 @@ public class Controller {
 
     public void openDialog() {
         System.out.println("Login: " + loginText.getText() + " Hasło: " + passwordText.getText());
+        Statement statement = MySqlConnector.getInstance().getNewStatement();
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM user WHERE name = '" + loginText.getText()+"'");
+
+            int counter = 0;
+
+                while (resultSet.next()) {
+                    String passwordFromDatabase = resultSet.getString("password");
+                    if(passwordFromDatabase.equals(passwordText.getText())){
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Logowanie");
+                        alert.setContentText("Zalogowałeś się poprawnie");
+                        alert.showAndWait();
+                    }else{
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Logowanie");
+                        alert.setContentText("Błędne hasło");
+                        alert.showAndWait();
+                    }
+                    counter++;
+                }
+           if(counter == 0) {
+               Alert alert = new Alert(Alert.AlertType.INFORMATION);
+               alert.setTitle("Logowanie");
+               alert.setContentText("Użytkownik nie istnieje");
+               alert.showAndWait();
+           }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
